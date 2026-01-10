2f99684359060ae9772551abb257cd1d9b3d9379
Krishan Patel <krishan711@gmail.com>
2026-01-10 21:17:21 +0000

    .

--- src/agentwalletkit/AWKAdapter.sol
+++ src/agentwalletkit/AWKAdapter.sol
@@ -1,4 +1,17 @@
 // SPDX-License-Identifier: MIT
+//
+//      _                    _ __        __    _ _      _   _  ___ _
+//     / \   __ _  ___ _ __ | |\ \      / /_ _| | | ___| |_| |/ (_) |_
+//    / _ \ / _` |/ _ \ '_ \| __\ \ /\ / / _` | | |/ _ \ __| ' /| | __|
+//   / ___ \ (_| |  __/ | | | |_ \ V  V / (_| | | |  __/ |_| . \| | |_
+//  /_/   \_\__, |\___|_| |_|\__| \_/\_/ \__,_|_|_|\___|\__|_|\_\_|\__|
+//          |___/
+//
+//  Build verifiably secure onchain agents
+//  https://agentwalletkit.tokenpage.xyz
+//
+//  For technical queries or guidance contact @krishan711
+//
 pragma solidity 0.8.28;
 
 import {AWKErrors} from "./AWKErrors.sol";
